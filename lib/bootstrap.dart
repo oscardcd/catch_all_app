e312b0bfc288f92d1ae66f6b9460fdd597f7b4de
oscardcd <oscardcd17@gmail.com>
2026-03-04 12:31:14 -0500

    feat: Implement initial dependency injection setup.

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,8 +1,10 @@
+import 'package:catch_all_app/core/injector/init.dart';
 import 'package:flutter/material.dart';
 
 import 'core/core.dart';
 
 Future<void> bootstrap() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await configureDependencies();
   runApp(const CatchAllApp());
 }

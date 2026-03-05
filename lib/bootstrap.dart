a3ef70ec574674f654e40392e9b3aefdecaa9d3a
oscardcd <oscardcd17@gmail.com>
2026-03-04 20:49:58 -0500

    feat: Implement Firebase authentication with a new AuthService abstraction and refactor data and domain layers.

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,6 +1,6 @@
-import 'package:catch_all_app/core/injector/init.dart';
-import 'package:flutter/material.dart';
+import 'package:catch_all_app/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:local_storage/local_storage.dart';
 
 import 'core/core.dart';
@@ -11,8 +11,10 @@ Future<void> bootstrap() async {
   // Initialize Local Storage via Hive
   await LocalStorage.instance.init();
 
-  // Initialize Firebase (run 'flutterfire configure' to generate firebase_options.dart)
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+    name: 'catch-all-app',
+  );
 
   await configureDependencies();
   runApp(const CatchAllApp());
